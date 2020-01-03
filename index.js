f4342f10b1870be9b912c73bc58277ac0d0b8e33
sky <cl98914@alibaba-inc.com>
2020-01-03 15:17:13 +0800

    feat: add killer export

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ exports.chalk = require('chalk');
 exports.shell = require('shelljs');
 exports.glob = require('glob');
 exports.httpserver = require('node-http-server');
+exports.killer = require('cross-port-killer');
 exports.opn = require('opn');
 Object.assign(exports, require('./lib/tool'));
